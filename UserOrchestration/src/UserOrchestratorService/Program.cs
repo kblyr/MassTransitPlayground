4a7023770df64c008f172a71d8fa6169ac5e96d4
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-12-10 22:14:08 +0800

    UserOrchestratorService

---
+++ UserOrchestration/src/UserOrchestratorService/Program.cs
@@ -0,0 +1,10 @@
+using UserOrchestration.UserOrchestratorService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
